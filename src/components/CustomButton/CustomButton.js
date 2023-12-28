eedb696ecf4472357af4c06755b94f0efc59d749
utkarsh_sharma_7 <utkarsh.sharma@unthinkable.co>
2023-12-28 12:01:22 +0530

    did changes in CustomButton

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Image } from "antd";
+import { Button } from "antd";
 
 import styles from "./CustomButton.module.scss";
 
@@ -8,7 +8,7 @@ const CustomButton = ({
   btnText,
   customStyle,
   iconStyles,
-  iconUrl,
+  IconElement,
   isBtnDisable,
   loading,
   onClick,
@@ -17,11 +17,7 @@ const CustomButton = ({
   return (
     <div>
       <Button
-        icon={
-          iconUrl ? (
-            <Image src={iconUrl} className={iconStyles} preview={false} />
-          ) : null
-        }
+        icon={IconElement ? <IconElement className={iconStyles} /> : null}
         className={[styles.btn, customStyle].join(" ")}
         disabled={isBtnDisable}
         {...{ onClick, loading }}
@@ -38,7 +34,7 @@ CustomButton.defaultProps = {
   btnText: "",
   customStyle: "",
   iconStyles: "",
-  iconUrl: "",
+  IconElement: null,
   isBtnDisable: false,
   loading: false,
   onClick: () => {},
@@ -49,7 +45,7 @@ CustomButton.propTypes = {
   btnText: PropTypes.string,
   customStyle: PropTypes.string,
   iconStyles: PropTypes.string,
-  iconUrl: PropTypes.string,
+  IconElement: PropTypes.node,
   isBtnDisable: PropTypes.bool,
   loading: PropTypes.bool,
   onClick: PropTypes.func,
