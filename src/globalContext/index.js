290b602d8425a42fc4b886a5c7e782060386a078
utkarsh_sharma_7 <utkarsh.sharma@unthinkable.co>
2023-12-29 09:28:39 +0530

    removed userDetails context

--- src/globalContext/index.js
+++ src/globalContext/index.js
@@ -9,7 +9,6 @@ const providers = [
   authProvider,
   dashboardProvider,
   localeProviders,
-  UserDetailsProvider,
 ];
 
 const combineProviders = (components) => {
--- src/globalContext/userDetails/types.js
+++
@@ -1,2 +0,0 @@
-export const SET_USER_DETAILS = "SET_USER_NAME";
-export const CLEAR_USER_DETAILS = "CLEAR_USER_NAME";
