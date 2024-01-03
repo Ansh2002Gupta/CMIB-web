13eba81d4bb1858cb721189c30375b5d2b415de1
abhayagrahary7080 <abhay.agrahary@unthinkable.co>
2024-01-03 12:20:53 +0530

    PR Changes

--- src/components/CustomTabs/CustomTabs.js
+++ src/components/CustomTabs/CustomTabs.js
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import PropTypes from "prop-types";
 import { Typography } from "antd";
 
 import styles from "./CustomTabs.module.scss";
 
 const CustomTabs = ({ activeTab, setActiveTab, tabs }) => {
-  const navigate = useNavigate();
-  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
   const tabClass = (tabKey) => {
     let classes = `${styles.tab}`;
     if (activeTab === tabKey) classes += ` ${styles.active}`;
@@ -16,25 +15,24 @@ const CustomTabs = ({ activeTab, setActiveTab, tabs }) => {
 
   const handleSelectTab = (tabName) => {
     setActiveTab(tabName);
-    navigate({
-      pathname: location.pathname,
-      search: `?tab=${tabName}`,
+    setSearchParams((params) => {
+      params.set("tab", tabName);
+      return params;
     });
   };
 
   useEffect(() => {
-    const queryParams = new URLSearchParams(location.search);
-    const tabQueryParam = queryParams.get("tab");
+    const tabQueryParam = searchParams.get("tab");
     if (tabs.some((tab) => tab.key === tabQueryParam)) {
       setActiveTab(tabQueryParam);
     } else {
       setActiveTab(tabs[0].key);
-      navigate({
-        pathname: location.pathname,
-        search: `?tab=${tabs[0].key}`,
+      setSearchParams((params) => {
+        params.set("tab", tabs[0].key);
+        return params;
       });
     }
-  }, [location.search, tabs, navigate, setActiveTab]);
+  }, [tabs, setActiveTab]);
 
   return (
     <div className={styles["tab-container"]}>
