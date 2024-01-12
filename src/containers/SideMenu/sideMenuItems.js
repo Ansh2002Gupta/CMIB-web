a871f5774b73bf46d11be23db4ad955dc6a8cff4
utkarsh_sharma_7 <utkarsh.sharma@unthinkable.co>
2024-01-12 16:53:05 +0530

    reverting my changes

--- src/containers/SideMenu/sideMenuItems.js
+++ src/containers/SideMenu/sideMenuItems.js
@@ -2,99 +2,99 @@ import { UserOutlined } from "@ant-design/icons";
 
 // TODO: need to add icons according to menu
 const controlMenu = [
-  { label: "Users", key: "/users", icon: <UserOutlined /> },
-  { label: "Notifications", key: "/notifications", icon: <UserOutlined /> },
-  { label: "Contact Us", key: "/contact-us", icon: <UserOutlined /> },
-  { label: "Session", key: "/session", icon: <UserOutlined /> },
-  { label: "Feedback", key: "/feedback", icon: <UserOutlined /> },
-  { label: "Testimonials", key: "/testimonials", icon: <UserOutlined /> },
+  { label: "Users", key: "users", icon: <UserOutlined /> },
+  { label: "Notifications", key: "notifications", icon: <UserOutlined /> },
+  { label: "Contact Us", key: "contact-us", icon: <UserOutlined /> },
+  { label: "Session", key: "session", icon: <UserOutlined /> },
+  { label: "Feedback", key: "feedback", icon: <UserOutlined /> },
+  { label: "Testimonials", key: "testimonials", icon: <UserOutlined /> },
   {
     label: "Registered Companies",
-    key: "/register-companies",
+    key: "register-companies",
     icon: <UserOutlined />,
   },
-  { label: "Activity Logs", key: "/activity-logs", icon: <UserOutlined /> },
+  { label: "Activity Logs", key: "activity-logs", icon: <UserOutlined /> },
 ];
 const newlyQualifiedPlacementsMenu = [
-  { label: "Dashboard", key: "/dashboard", icon: <UserOutlined /> },
+  { label: "Dashboard", key: "dashboard", icon: <UserOutlined /> },
   {
     label: "Round 1 Placements",
-    key: "/round-1-placements",
+    key: "round-1-placements",
     icon: <UserOutlined />,
   },
   {
     label: "Round 2 Placements",
-    key: "/round-2-placements",
+    key: "round-2-placements",
     icon: <UserOutlined />,
   },
   {
     label: "Global configurations",
-    key: "/global-configurations",
+    key: "global-configurations",
     icon: <UserOutlined />,
   },
-  { label: "Companies", key: "/companies", icon: <UserOutlined /> },
+  { label: "Companies", key: "companies", icon: <UserOutlined /> },
 ];
 const caJobsMenu = [
-  { label: "Dashboard", key: "/dashboard", icon: <UserOutlined /> },
-  { label: "Subscriptions", key: "/subscriptions", icon: <UserOutlined /> },
+  { label: "Dashboard", key: "dashboard", icon: <UserOutlined /> },
+  { label: "Subscriptions", key: "subscriptions", icon: <UserOutlined /> },
   {
     label: "Global Configurations",
     key: "/global-configurations",
     icon: <UserOutlined />,
   },
-  { label: "Payments", key: "/payments", icon: <UserOutlined /> },
-  { label: "Candidates", key: "/candidates", icon: <UserOutlined /> },
-  { label: "Companies", key: "/companies", icon: <UserOutlined /> },
-  { label: "All Jobs", key: "/all-jobs", icon: <UserOutlined /> },
-  { label: "Report", key: "/report", icon: <UserOutlined /> },
+  { label: "Payments", key: "payments", icon: <UserOutlined /> },
+  { label: "Candidates", key: "candidates", icon: <UserOutlined /> },
+  { label: "Companies", key: "companies", icon: <UserOutlined /> },
+  { label: "All Jobs", key: "all-jobs", icon: <UserOutlined /> },
+  { label: "Report", key: "report", icon: <UserOutlined /> },
 ];
 const experiencedMembersMenu = [
-  { label: "Dashboard", key: "/dashboard", icon: <UserOutlined /> },
-  { label: "Payments", key: "/payments", icon: <UserOutlined /> },
-  { label: "Candidates", key: "/candidates", icon: <UserOutlined /> },
-  { label: "Companies", key: "/companies", icon: <UserOutlined /> },
+  { label: "Dashboard", key: "dashboard", icon: <UserOutlined /> },
+  { label: "Payments", key: "payments", icon: <UserOutlined /> },
+  { label: "Candidates", key: "candidates", icon: <UserOutlined /> },
+  { label: "Companies", key: "companies", icon: <UserOutlined /> },
   {
     label: "Global Configurations",
-    key: "/global-configurations",
+    key: "global-configurations",
     icon: <UserOutlined />,
   },
-  { label: "Session", key: "/session", icon: <UserOutlined /> },
-  { label: "Roster", key: "/roster", icon: <UserOutlined /> },
-  { label: "Report", key: "/report", icon: <UserOutlined /> },
+  { label: "Session", key: "session", icon: <UserOutlined /> },
+  { label: "Roster", key: "roster", icon: <UserOutlined /> },
+  { label: "Report", key: "report", icon: <UserOutlined /> },
 ];
 // TODO: need to add role based menu
 const modules = [
   {
     label: "Control",
-    key: "/control",
+    key: "control",
     children: controlMenu,
   },
   {
     label: "Newly Qualified Placements",
-    key: "/newly-qualified-placements",
+    key: "newly-qualified-placements",
     children: newlyQualifiedPlacementsMenu,
   },
   {
     label: "CA Jobs",
-    key: "/ca-jobs",
+    key: "ca-jobs",
     children: caJobsMenu,
   },
   {
     label: "Experienced Members",
-    key: "/experienced-members",
+    key: "experienced-members",
     subMenu: [
       {
-        key: "/career-ascent",
+        key: "career-ascent",
         label: "Career Ascent",
         children: experiencedMembersMenu,
       },
       {
-        key: "/women-part-time",
+        key: "women-part-time",
         label: "Women PartTime",
         children: experiencedMembersMenu,
       },
       {
-        key: "/overseas-chapters",
+        key: "overseas-chapters",
         label: "Overseas Chapters",
         children: experiencedMembersMenu,
       },
