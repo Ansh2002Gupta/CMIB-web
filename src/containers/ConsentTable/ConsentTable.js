22b1ceadc92058bb553c12e603af9285bd0a2f2c
abhayagrahary7080 <abhay.agrahary@unthinkable.co>
2024-01-12 19:36:42 +0530

    pR changes

--- src/containers/ConsentTable/ConsentTable.js
+++ src/containers/ConsentTable/ConsentTable.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useIntl } from "react-intl";
 import { Table } from "antd";
 
-import useRenderColumn from "../../core/hooks/useRenderColumn/useRenderColumn";
-import DataTable from "../../components/DataTable/DataTable";
+import {
+  useConsentTableColumns,
+  onDateChange as handleDateChange,
+} from "./ConsentTableConfig";
 import styles from "./ConsentTable.module.scss";
 import "./Override.css";
 
@@ -15,155 +16,11 @@ const ConsentTable = ({
   setTableData,
   tableData,
 }) => {
-  const { renderColumn } = useRenderColumn();
-  const intl = useIntl();
-
-  const onDateChange = (record, key, value) => {
-    const index = tableData.findIndex((item) => item.sNo === record.sNo);
-    const newData = [...tableData];
-    newData[index][key] = value;
-    setTableData(newData);
-  };
-
-  const columns = registration
-    ? [
-        renderColumn({
-          title: intl.formatMessage({ id: "label.sNo" }),
-          dataIndex: "sNo",
-          key: "sNo",
-          renderText: {
-            visible: true,
-            includeDotAfterText: true,
-            textStyles: styles.textStyles,
-          },
-        }),
-        renderColumn({
-          title: intl.formatMessage({ id: "label.centreName" }),
-          dataIndex: "centreName",
-          key: "centreName",
-          renderText: { isTextBold: true, visible: true },
-        }),
-        renderColumn({
-          title: intl.formatMessage({ id: "label.lastRegistrationDate" }),
-          dataIndex: "lastRegistrationDate",
-          key: "lastRegistrationDate",
-          isRequiredField: true,
-          renderDateTime: {
-            visible: isEdit,
-            type: "date",
-            placeholder: intl.formatMessage({
-              id: "label.placeholder.lastRegistrationDate",
-            }),
-            customTimeStyle: styles.customTimeStyle,
-            onChange: (date, record) =>
-              onDateChange(record, "lastRegistrationDate", date),
-          },
-          renderText: { visible: !isEdit, isTypeDate: true },
-        }),
-        renderColumn({
-          title: intl.formatMessage({ id: "label.psychometricTestDate" }),
-          dataIndex: "psychometricTestDate",
-          isRequiredField: true,
-          key: "psychometricTestDate",
-          renderDateTime: {
-            visible: isEdit,
-            type: "date",
-            placeholder: intl.formatMessage({
-              id: "label.placeholder.psychometricTestDate",
-            }),
-            customTimeStyle: styles.customTimeStyle,
-            onChange: (date, record) =>
-              onDateChange(record, "psychometricTestDate", date),
-          },
-          renderText: { visible: true, isTypeDate: true },
-        }),
-      ]
-    : [
-        renderColumn({
-          title: intl.formatMessage({ id: "label.sNo" }),
-          dataIndex: "sNo",
-          key: "sNo",
-          renderText: {
-            visible: true,
-            includeDotAfterText: true,
-            textStyles: styles.textStyles,
-          },
-        }),
-        renderColumn({
-          title: intl.formatMessage({ id: "label.centreName" }),
-          dataIndex: "centreName",
-          key: "centreName",
-          renderText: { isTextBold: true, visible: true },
-        }),
-        renderColumn({
-          title: intl.formatMessage({ id: "label.companyStartDate" }),
-          dataIndex: "companyStartDate",
-          key: "companyStartDate",
-          isRequiredField: true,
-          renderDateTime: {
-            visible: isEdit,
-            type: "date",
-            placeholder: intl.formatMessage({
-              id: "label.placeholder.companyStartDate",
-            }),
-            customTimeStyle: styles.customTimeStyle,
-            onChange: (date, record) =>
-              onDateChange(record, "companyStartDate", date),
-          },
-          renderText: { visible: !isEdit, isTypeDate: true },
-        }),
-        renderColumn({
-          title: intl.formatMessage({ id: "label.consentFromDate" }),
-          dataIndex: "companyEndDate",
-          isRequiredField: true,
-          key: "companyEndDate",
-          renderDateTime: {
-            visible: isEdit,
-            type: "date",
-            placeholder: intl.formatMessage({
-              id: "label.placeholder.companyEndDate",
-            }),
-            customTimeStyle: styles.customTimeStyle,
-            onChange: (date, record) =>
-              onDateChange(record, "companyEndDate", date),
-          },
-          renderText: { visible: true, isTypeDate: true },
-        }),
-        renderColumn({
-          title: intl.formatMessage({ id: "label.consentFromDate" }),
-          dataIndex: "consentFromDate",
-          isRequiredField: true,
-          key: "consentFromDate",
-          renderDateTime: {
-            visible: isEdit,
-            type: "date",
-            placeholder: intl.formatMessage({
-              id: "label.placeholder.consentFromDate",
-            }),
-            customTimeStyle: styles.customTimeStyle,
-            onChange: (date, record) =>
-              onDateChange(record, "consentFromDate", date),
-          },
-          renderText: { visible: true, isTypeDate: true },
-        }),
-        renderColumn({
-          title: intl.formatMessage({ id: "label.consentToDate" }),
-          dataIndex: "consentToDate",
-          isRequiredField: true,
-          key: "consentToDate",
-          renderDateTime: {
-            visible: isEdit,
-            type: "date",
-            placeholder: intl.formatMessage({
-              id: "label.placeholder.consentToDate",
-            }),
-            customTimeStyle: styles.customTimeStyle,
-            onChange: (date, record) =>
-              onDateChange(record, "consentToDate", date),
-          },
-          renderText: { visible: true, isTypeDate: true },
-        }),
-      ];
+  const columns = useConsentTableColumns(
+    isEdit,
+    registration,
+    handleDateChange(tableData, setTableData)
+  );
 
   return (
     <Table
@@ -175,17 +32,6 @@ const ConsentTable = ({
       className={[styles.table, "customConsentTable"]}
       rowKey="id"
     />
-    // <DataTable
-    //   {...{
-    //     columns,
-    //     // current,
-    //     // pageSize,
-    //     // onChangePageSize,
-    //     // onChangeCurrentPage,
-    //   }}
-    //   currentDataLength={tableData.length}
-    //   originalData={tableData}
-    // />
   );
 };
 
---
+++ src/containers/ConsentTable/ConsentTableConfig.js
@@ -0,0 +1,162 @@
+import React from "react";
+import { useIntl } from "react-intl";
+
+import useRenderColumn from "../../core/hooks/useRenderColumn/useRenderColumn";
+import styles from "./ConsentTable.module.scss";
+import "./Override.css";
+
+export const useConsentTableColumns = (isEdit, registration, onDateChange) => {
+  const { renderColumn } = useRenderColumn();
+  const intl = useIntl();
+
+  // Define the columns based on registration prop and isEdit state
+  const columns = registration
+    ? [
+        renderColumn({
+          title: intl.formatMessage({ id: "label.sNo" }),
+          dataIndex: "sNo",
+          key: "sNo",
+          renderText: {
+            visible: true,
+            includeDotAfterText: true,
+            textStyles: styles.textStyles,
+          },
+        }),
+        renderColumn({
+          title: intl.formatMessage({ id: "label.centreName" }),
+          dataIndex: "centreName",
+          key: "centreName",
+          renderText: { isTextBold: true, visible: true },
+        }),
+        renderColumn({
+          title: intl.formatMessage({ id: "label.lastRegistrationDate" }),
+          dataIndex: "lastRegistrationDate",
+          key: "lastRegistrationDate",
+          isRequiredField: true,
+          renderDateTime: {
+            visible: isEdit,
+            type: "date",
+            placeholder: intl.formatMessage({
+              id: "label.placeholder.lastRegistrationDate",
+            }),
+            customTimeStyle: styles.customTimeStyle,
+            onChange: (date, record) =>
+              onDateChange(record, "lastRegistrationDate", date),
+          },
+          renderText: { visible: !isEdit, isTypeDate: true },
+        }),
+        renderColumn({
+          title: intl.formatMessage({ id: "label.psychometricTestDate" }),
+          dataIndex: "psychometricTestDate",
+          isRequiredField: true,
+          key: "psychometricTestDate",
+          renderDateTime: {
+            visible: isEdit,
+            type: "date",
+            placeholder: intl.formatMessage({
+              id: "label.placeholder.psychometricTestDate",
+            }),
+            customTimeStyle: styles.customTimeStyle,
+            onChange: (date, record) =>
+              onDateChange(record, "psychometricTestDate", date),
+          },
+          renderText: { visible: true, isTypeDate: true },
+        }),
+      ]
+    : [
+        renderColumn({
+          title: intl.formatMessage({ id: "label.sNo" }),
+          dataIndex: "sNo",
+          key: "sNo",
+          renderText: {
+            visible: true,
+            includeDotAfterText: true,
+            textStyles: styles.textStyles,
+          },
+        }),
+        renderColumn({
+          title: intl.formatMessage({ id: "label.centreName" }),
+          dataIndex: "centreName",
+          key: "centreName",
+          renderText: { isTextBold: true, visible: true },
+        }),
+        renderColumn({
+          title: intl.formatMessage({ id: "label.companyStartDate" }),
+          dataIndex: "companyStartDate",
+          key: "companyStartDate",
+          isRequiredField: true,
+          renderDateTime: {
+            visible: isEdit,
+            type: "date",
+            placeholder: intl.formatMessage({
+              id: "label.placeholder.companyStartDate",
+            }),
+            customTimeStyle: styles.customTimeStyle,
+            onChange: (date, record) =>
+              onDateChange(record, "companyStartDate", date),
+          },
+          renderText: { visible: !isEdit, isTypeDate: true },
+        }),
+        renderColumn({
+          title: intl.formatMessage({ id: "label.consentFromDate" }),
+          dataIndex: "companyEndDate",
+          isRequiredField: true,
+          key: "companyEndDate",
+          renderDateTime: {
+            visible: isEdit,
+            type: "date",
+            placeholder: intl.formatMessage({
+              id: "label.placeholder.companyEndDate",
+            }),
+            customTimeStyle: styles.customTimeStyle,
+            onChange: (date, record) =>
+              onDateChange(record, "companyEndDate", date),
+          },
+          renderText: { visible: true, isTypeDate: true },
+        }),
+        renderColumn({
+          title: intl.formatMessage({ id: "label.consentFromDate" }),
+          dataIndex: "consentFromDate",
+          isRequiredField: true,
+          key: "consentFromDate",
+          renderDateTime: {
+            visible: isEdit,
+            type: "date",
+            placeholder: intl.formatMessage({
+              id: "label.placeholder.consentFromDate",
+            }),
+            customTimeStyle: styles.customTimeStyle,
+            onChange: (date, record) =>
+              onDateChange(record, "consentFromDate", date),
+          },
+          renderText: { visible: true, isTypeDate: true },
+        }),
+        renderColumn({
+          title: intl.formatMessage({ id: "label.consentToDate" }),
+          dataIndex: "consentToDate",
+          isRequiredField: true,
+          key: "consentToDate",
+          renderDateTime: {
+            visible: isEdit,
+            type: "date",
+            placeholder: intl.formatMessage({
+              id: "label.placeholder.consentToDate",
+            }),
+            customTimeStyle: styles.customTimeStyle,
+            onChange: (date, record) =>
+              onDateChange(record, "consentToDate", date),
+          },
+          renderText: { visible: true, isTypeDate: true },
+        }),
+      ];
+
+  return columns;
+};
+
+export const onDateChange =
+  (tableData, setTableData) => (record, key, value) => {
+    const index = tableData.findIndex((item) => item.sNo === record.sNo);
+    const newData = [...tableData];
+    newData[index][key] = value;
+    setTableData(newData);
+  };
