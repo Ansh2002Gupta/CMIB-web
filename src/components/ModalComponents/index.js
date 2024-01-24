c3391895dbd309bf8dc0c05d9001f7994427f45c
utkarsh_sharma_7 <utkarsh.sharma@unthinkable.co>
2024-01-24 15:12:53 +0530

    .

---
+++ src/components/ModalComponents/index.js
@@ -0,0 +1,10 @@
+import LogoutModal from './LogoutModal';
+import ViewProfileModal from './ViewProfileModal';
+import ChangeProfileModal from './ChangeProfileModal';
+
+const ModalComponents = {
+  LogoutModal,
+  ViewProfileModal,
+  ChangeProfileModal,
+};
+export default ModalComponents;
