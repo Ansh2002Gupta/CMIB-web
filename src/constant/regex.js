14a9acf2eeed2a303451420c1199062a3827fae8
abhayagrahary7080 <abhay.agrahary@unthinkable.co>
2024-02-18 00:12:27 +0530

    Session Condition added

--- src/constant/regex.js
+++ src/constant/regex.js
@@ -7,7 +7,7 @@ export const AT_LEAST_ONE_SPECIAL_CHARACTER = /(?=.*[!?.@#$%^&+=])/;
 export const AT_LEAST_SIX_CHARACTERS_REGEX = /.{6,}/;
 export const BANK_ACC_NUMBER_REGEX = /^\d{9,18}$/;
 export const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-export const HSN_SAC_CODE_REGEX = /^(?:\d{2}|\d{4}|\d{6}|\d{8})$/;
+export const HSN_SAC_CODE_REGEX = /^(?:\d{6}|\d{8})$/;
 export const IFSC_CODE_REGEX = /^[A-Z]{4}0[A-Z0-9]{6}$/;
 export const MOBILE_NO_REGEX = /^[789]\d{9}$/;
 export const NUMERIC_VALUE_REGEX = /^\d+$/;
