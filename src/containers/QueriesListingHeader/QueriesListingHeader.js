835a658f613b397461e3b5df9035c5c76f658f66
abhayagrahary7080 <abhay.agrahary@unthinkable.co>
2024-02-20 14:08:24 +0530

    View Query Bug Resolved

--- src/containers/QueriesListingHeader/QueriesListingHeader.js
+++ src/containers/QueriesListingHeader/QueriesListingHeader.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { useIntl } from "react-intl";
 
 import ContentHeader from "../ContentHeader/ContentHeader";
+import { UserDetailContext } from "../../globalContext/userDetail/userDetailProvider";
+import { userDetailToast } from "../../globalContext/userDetail/userDetailActions";
+import useShowNotification from "../../core/hooks/useShowNotification";
 import styles from "./QueriesListingHeader.module.scss";
 
 const QueriesListingHeader = () => {
   const intl = useIntl();
+  const { showNotification, notificationContextHolder } = useShowNotification();
+  const [userDetailState, setUserDetailDispatch] =
+    useContext(UserDetailContext);
+
+  useEffect(() => {
+    if (userDetailState?.isUserSuccessfullyAdded) {
+      showNotification({
+        text: intl.formatMessage({
+          id: "label.noDataFound",
+        }),
+        type: "error",
+      });
+      setUserDetailDispatch(userDetailToast(false));
+    }
+  }, [userDetailState]);
 
   return (
     <div className={styles.headerBox}>
+      {notificationContextHolder}
       <ContentHeader
         headerText={intl.formatMessage({
           id: "label.queries",
