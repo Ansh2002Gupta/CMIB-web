a1a8af5481a8a0c9e413297e8b978071bb329340
Kashish8888 <kashish@skizaa.com>
2024-02-29 08:56:05 +0530

    Updated breadCrumbs

--- src/components/BreadCrumbs/BreadCrumbs.js
+++ src/components/BreadCrumbs/BreadCrumbs.js
@@ -2,7 +2,9 @@ import React from "react";
 import { useIntl } from "react-intl";
 import { useLocation, useSearchParams } from "react-router-dom";
 import { Breadcrumb } from "antd";
+
 import useNavigateScreen from "../../core/hooks/useNavigateScreen";
+import { SESSION_PATHS } from "../../constant/constant";
 import styles from "./BreadCrumbs.module.scss";
 
 const BreadCrumbs = () => {
@@ -20,13 +22,36 @@ const BreadCrumbs = () => {
   const pathSegments = segments.slice(2, -1);
   const { navigateScreen: navigate } = useNavigateScreen();
 
+  const getLabelForPath = (path, isEdit) => {
+    const sessionPathToLabelId = {
+      "setup-centers": "label.session.setupCenters.details",
+      "orientation-centers": "label.session.orientationCenters.details",
+      "setup-mock-interview": "label.session.setupMockInterview.details",
+      "consent-marking": "label.session.consentMarking.details",
+      "campus-interview": "label.session.campusInterview.details",
+      users: "label.users.details",
+    };
+
+    if (SESSION_PATHS.includes(path)) {
+      return intl.formatMessage({ id: `label.session.${path}` });
+    }
+    if (path === "details" && isEdit) {
+      return intl.formatMessage({ id: "label.path.editDetails" });
+    }
+    if (path === "details") {
+      const relevantSegment = pathSegments.find(
+        (segment) => segment === "users" || SESSION_PATHS.includes(segment)
+      );
+      const labelId = sessionPathToLabelId[relevantSegment] || "";
+      return intl.formatMessage({ id: labelId });
+    }
+    return intl.formatMessage({ id: `label.path.${path}` });
+  };
+
   const breadcrumbItems = pathSegments
     .map((item, index) => {
       const isLastItem = index === pathSegments.length - 1;
-      const itemName =
-        item === "details" && isEdit
-          ? intl.formatMessage({ id: "label.path.editDetails" })
-          : intl.formatMessage({ id: `label.path.${item}` });
+      const itemName = getLabelForPath(item, isEdit);
 
       const onClick = !isLastItem
         ? () => navigate(index - (pathSegments.length - 1))
@@ -42,7 +67,7 @@ const BreadCrumbs = () => {
         </Breadcrumb.Item>
       );
     })
-    .filter((item) => pathSegments.length > 1);
+    .filter(() => pathSegments.length > 1);
 
   return (
     <div className={styles.mainContainer}>
