5ff42199feb7c9985c46741576400a33658da300
Kashish8888 <kashish@skizaa.com>
2024-03-03 14:18:53 +0530

    minor view mode enhancements

--- src/containers/CentreTable/CentreTable.js
+++ src/containers/CentreTable/CentreTable.js
@@ -322,7 +322,7 @@ const CentreTable = ({
       title: intl.formatMessage({ id: "centre.scheduleDate" }),
       dataIndex: "scheduleDate",
       key: "scheduleDate",
-      renderText: { visible: true },
+      renderText: { visible: true, isTypeDate: true },
     }),
     renderColumn({
       title: intl.formatMessage({ id: "centre.participationFee" }),
