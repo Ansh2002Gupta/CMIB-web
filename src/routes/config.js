41af13ff603343477b04280f87a4a1ee3093dc5d
Lakshay <lakshay.gupta@unthinkable.co>
2024-03-07 13:06:05 +0530

    added the foundation for writing the ca jobs code

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,3 +1,4 @@
+import CaJobsConfigurations from "../views/CAJobs/CaJobsConfigurations";
 import Companies from "../views/Companies";
 import CompaniesDetails from "../views/CompaniesDetails";
 import Configurations from "../views/Configurations/Configurations";
@@ -27,6 +28,7 @@ import withPrivateAccess from "../hocs/withPrivateAccess";
 import withPublicAccess from "../hocs/withPublicAccess";
 import UserDetails from "../views/UserDetails";
 import QueryDetails from "../views/QueryDetails";
+
 import {
   ADD,
   ADD_NEW_USER_DETAILS,
@@ -187,7 +189,7 @@ const config = [
       },
       {
         viewPath: CONFIGURE_CENTRES,
-        element: <ConfigureCentres />,
+        element: <CaJobsConfigurations />,
       },
       {
         viewPath: CONFIGURE_CENTRES + ADD,
