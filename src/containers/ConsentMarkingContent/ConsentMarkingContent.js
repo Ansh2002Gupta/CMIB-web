d701d49a83a67ea79d706a35a3eb497d9d46bc02
abhayagrahary7080 <abhay.agrahary@unthinkable.co>
2024-03-14 13:50:05 +0530

    Reduce size

--- src/containers/ConsentMarkingContent/ConsentMarkingContent.js
+++ src/containers/ConsentMarkingContent/ConsentMarkingContent.js
@@ -1,5 +1,4 @@
-import React, { useCallback, useContext, useState, useEffect } from "react";
-import dayjs from "dayjs";
+import React, { useCallback, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useIntl } from "react-intl";
 
@@ -12,9 +11,16 @@ import CustomGrid from "../../components/CustomGrid";
 import CustomLoader from "../../components/CustomLoader/CustomLoader";
 import CustomTabs from "../../components/CustomTabs";
 import EditButton from "../../components/EditButton";
-import { UserProfileContext } from "../../globalContext/userProfile/userProfileProvider";
+import {
+  disabledDate,
+  handleInputChange,
+  onDateChange,
+  useIntitalDataAndError,
+  validate,
+  NQCA_REGISTRATION_DATE_FIELDS,
+  OTHER_MODULES_REGISTRATION_DATE_FIELDS,
+} from "./ConsentMarkingConfig";
 import useShowNotification from "../../core/hooks/useShowNotification";
-import { compareTwoDayjsDates, isNotAFutureDate } from "../../constant/utils";
 import { urlService } from "../../Utils/urlService";
 import { usePut } from "../../core/hooks/useApiRequest";
 import {
@@ -50,30 +56,6 @@ const ConsentMarkingContent = ({
   const { showNotification, notificationContextHolder } = useShowNotification();
   const [isRegistrationDateEdit, setIsRegistrationDateEdit] = useState(false);
   const [isTableDateEdit, setIsTableDateEdit] = useState(false);
-
-  const onDateChange = (
-    record,
-    key,
-    value,
-    tableData,
-    setTableData,
-    setErrors
-  ) => {
-    const index = tableData.findIndex((item) => item.id === record.id);
-    const newData = [...tableData];
-    newData[index][key] = value && dayjs(value).format("YYYY-MM-DD");
-    setTableData(newData);
-    if (!value) {
-      handleError(
-        key,
-        intl.formatMessage({ id: "label.error.fieldEmpty" }),
-        index,
-        setErrors
-      );
-    } else {
-      handleError(key, "", index, setErrors);
-    }
-  };
 
   const {
     makeRequest: updateRegistrationDate,
@@ -125,81 +107,18 @@ const ConsentMarkingContent = ({
   const isNqca =
     currentlySelectedModuleKey === MODULE_KEYS?.NEWLY_QUALIFIED_PLACEMENTS_KEY;
 
-  const roundOneInitialData = consentRoundOneData?.map((item) => ({
-    ...item,
-    sNo: item.id,
-    centre_name: item.centre_name,
-    company_shortlisting_start_date: item.company_shortlisting_start_date
-      ? item.company_shortlisting_start_date
-      : null,
-    company_shortlisting_end_date: item.company_shortlisting_end_date
-      ? item.company_shortlisting_end_date
-      : null,
-    candidate_consent_marking_start_date:
-      item.candidate_consent_marking_start_date
-        ? item.candidate_consent_marking_start_date
-        : null,
-    candidate_consent_marking_end_date: item.candidate_consent_marking_end_date
-      ? item.candidate_consent_marking_end_date
-      : null,
-  }));
-
-  const roundTwoInitialData = consentRoundTwoData?.map((item) => ({
-    ...item,
-    id: item.id,
-    centre_name: item.centre_name,
-    company_shortlisting_start_date: item.company_shortlisting_start_date
-      ? item.company_shortlisting_start_date
-      : null,
-    company_shortlisting_end_date: item.company_shortlisting_end_date
-      ? item.company_shortlisting_end_date
-      : null,
-    candidate_consent_marking_start_date:
-      item.candidate_consent_marking_start_date
-        ? item.candidate_consent_marking_start_date
-        : null,
-    candidate_consent_marking_end_date: item.candidate_consent_marking_end_date
-      ? item.candidate_consent_marking_end_date
-      : null,
-  }));
-
-  const lastRegistrationInitialData = lastRegistrationDatesData?.map(
-    (item) => ({
-      ...item,
-      id: item.id,
-      name: item.name,
-      company_reg_end_date: item.company_reg_end_date
-        ? item.company_reg_end_date
-        : null,
-      psychometric_test_date: item.psychometric_test_date
-        ? item.psychometric_test_date
-        : null,
-    })
-  );
-
-  const lastRegistrationInitialError = lastRegistrationDatesData?.map(
-    (item) => ({
-      name: "",
-      company_reg_end_date: "",
-      psychometric_test_date: "",
-    })
-  );
-
-  const roundOneInitialError = consentRoundOneData?.map((item) => ({
-    centre_name: "",
-    company_shortlisting_start_date: "",
-    company_shortlisting_end_date: "",
-    candidate_consent_marking_start_date: "",
-    candidate_consent_marking_end_date: "",
-  }));
-
-  const roundTwoInitialError = consentRoundTwoData?.map((item) => ({
-    centre_name: "",
-    company_shortlisting_start_date: "",
-    company_shortlisting_end_date: "",
-    candidate_consent_marking_start_date: "",
-    candidate_consent_marking_end_date: "",
-  }));
+  const {
+    roundOneInitialData,
+    roundTwoInitialData,
+    lastRegistrationInitialData,
+    roundOneInitialError,
+    roundTwoInitialError,
+    lastRegistrationInitialError,
+  } = useIntitalDataAndError({
+    consentRoundOneData,
+    consentRoundTwoData,
+    lastRegistrationDatesData,
+  });
 
   const [lastRegistrationError, setLastRegistrationError] = useState(
     lastRegistrationInitialError
@@ -214,111 +133,6 @@ const ConsentMarkingContent = ({
   const [lastRegistrationTableData, setLastRegistrationTableData] = useState(
     lastRegistrationInitialData
   );
-
-  const disabledDate = (key, current) => {
-    if (key === "company_reg_start_date") {
-      return (
-        isNotAFutureDate(current) ||
-        compareTwoDayjsDates({
-          current: current,
-          date: registrationDatesData["candidate_reg_end_date_bg_centre"],
-          checkForFuture: true,
-        }) ||
-        compareTwoDayjsDates({
-          current: current,
-          date: registrationDatesData["candidate_reg_end_date_sm_centre"],
-          checkForFuture: true,
-        })
-      );
-    }
-    if (key === "candidate_reg_start_date") {
-      return (
-        isNotAFutureDate(current) ||
-        compareTwoDayjsDates({
-          current: current,
-          date: registrationDatesData["candidate_reg_end_date_bg_centre"],
-          checkForFuture: true,
-        }) ||
-        compareTwoDayjsDates({
-          current: current,
-          date: registrationDatesData["candidate_reg_end_date_sm_centre"],
-          checkForFuture: true,
-        })
-      );
-    }
-    if (key === "candidate_reg_end_date_bg_centre")
-      return (
-        isNotAFutureDate(current) ||
-        compareTwoDayjsDates({
-          current: current,
-          date: registrationDatesData["company_reg_start_date"],
-          checkForFuture: false,
-        }) ||
-        compareTwoDayjsDates({
-          current: current,
-          date: registrationDatesData["candidate_reg_start_date"],
-          checkForFuture: false,
-        }) ||
-        compareTwoDayjsDates({
-          current: current,
-          date: registrationDatesData["candidate_reg_end_date_sm_centre"],
-          checkForFuture: true,
-        })
-      );
-    return (
-      isNotAFutureDate(current) ||
-      compareTwoDayjsDates({
-        current: current,
-        date: registrationDatesData["company_reg_start_date"],
-        checkForFuture: false,
-      }) ||
-      compareTwoDayjsDates({
-        current: current,
-        date: registrationDatesData["candidate_reg_start_date"],
-        checkForFuture: false,
-      }) ||
-      compareTwoDayjsDates({
-        current: current,
-        date:
-          registrationDatesData["candidate_reg_end_date_bg_centre"] ||
-          registrationDatesData["candidate_reg_start_date"],
-        checkForFuture: false,
-      })
-    );
-  };
-  const NQCA_REGISTRATION_DATE_FIELDS = [
-    {
-      id: 1,
-      labeIntl: "company_reg_start_date",
-    },
-    {
-      id: 2,
-      labeIntl: "candidate_reg_start_date",
-    },
-    {
-      id: 3,
-      labeIntl: "candidate_reg_end_date_bg_centre",
-    },
-    {
-      id: 4,
-      labeIntl: "candidate_reg_end_date_sm_centre",
-    },
-  ];
-
-  const OTHER_MODULES_REGISTRATION_DATE_FIELDS = [
-    {
-      id: 1,
-      labeIntl: "company_reg_start_date",
-    },
-    {
-      id: 2,
-      labeIntl: "candidate_reg_start_date",
-    },
-    {
-      id: 3,
-      labeIntl: "candidate_reg_end_date_bg_centre",
-    },
-  ];
 
   const registrationDates = isNqca
     ? NQCA_REGISTRATION_DATE_FIELDS
@@ -342,6 +156,7 @@ const ConsentMarkingContent = ({
           tableData={lastRegistrationTableData}
           onDateChange={(record, key, value) => {
             onDateChange(
+              intl,
               record,
               key,
               value,
@@ -368,6 +183,7 @@ const ConsentMarkingContent = ({
           tableData={roundOneTableData}
           onDateChange={(record, key, value) => {
             onDateChange(
+              intl,
               record,
               key,
               value,
@@ -394,6 +210,7 @@ const ConsentMarkingContent = ({
             tableData={roundTwoTableData}
             onDateChange={(record, key, value) => {
               onDateChange(
+                intl,
                 record,
                 key,
                 value,
@@ -406,135 +223,8 @@ const ConsentMarkingContent = ({
         ),
       },
   ];
+
   const activeTabChildren = tabItems.find((tab) => tab.key === activeTab);
-
-  const handleInputChange = (value, name) => {
-    setRegistrationDatesData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value && dayjs(value).format("YYYY-MM-DD"),
-    }));
-  };
-
-  const handleError = (key, error, index, setErrors) => {
-    setErrors((prevTableError) => {
-      const newTableError = [...prevTableError];
-      newTableError[index] = {
-        ...newTableError[index],
-        [key]: error,
-      };
-      return newTableError;
-    });
-  };
-
-  const validate = () => {
-    let errorCount = 0;
-    if (activeTab === "1") {
-      lastRegistrationTableData.map((item, index) => {
-        if (!item?.company_reg_end_date) {
-          errorCount++;
-          handleError(
-            "company_reg_end_date",
-            intl.formatMessage({ id: "label.error.fieldEmpty" }),
-            index,
-            setLastRegistrationError
-          );
-        }
-        if (!item?.psychometric_test_date) {
-          errorCount++;
-          handleError(
-            "psychometric_test_date",
-            intl.formatMessage({ id: "label.error.fieldEmpty" }),
-            index,
-            setLastRegistrationError
-          );
-        }
-      });
-    }
-    if (activeTab === "2") {
-      roundOneTableData.map((item, index) => {
-        if (!item?.candidate_consent_marking_end_date) {
-          errorCount++;
-          handleError(
-            "candidate_consent_marking_end_date",
-            intl.formatMessage({ id: "label.error.fieldEmpty" }),
-            index,
-            setRoundOneError
-          );
-        }
-        if (!item?.candidate_consent_marking_start_date) {
-          errorCount++;
-          handleError(
-            "candidate_consent_marking_start_date",
-            intl.formatMessage({ id: "label.error.fieldEmpty" }),
-            index,
-            setRoundOneError
-          );
-        }
-        if (!item?.company_shortlisting_end_date) {
-          errorCount++;
-          handleError(
-            "company_shortlisting_end_date",
-            intl.formatMessage({ id: "label.error.fieldEmpty" }),
-            index,
-            setRoundOneError
-          );
-        }
-        if (!item?.company_shortlisting_start_date) {
-          errorCount++;
-          handleError(
-            "company_shortlisting_start_date",
-            intl.formatMessage({ id: "label.error.fieldEmpty" }),
-            index,
-            setRoundOneError
-          );
-        }
-      });
-    }
-    if (activeTab === "3") {
-      roundTwoTableData.map((item, index) => {
-        if (!item?.candidate_consent_marking_end_date) {
-          errorCount++;
-          handleError(
-            "candidate_consent_marking_end_date",
-            intl.formatMessage({ id: "label.error.fieldEmpty" }),
-            index,
-            setRoundTwoError
-          );
-        }
-        if (!item?.candidate_consent_marking_start_date) {
-          errorCount++;
-          handleError(
-            "candidate_consent_marking_start_date",
-            intl.formatMessage({ id: "label.error.fieldEmpty" }),
-            index,
-            setRoundTwoError
-          );
-        }
-        if (!item?.company_shortlisting_end_date) {
-          errorCount++;
-          handleError(
-            "company_shortlisting_end_date",
-            intl.formatMessage({ id: "label.error.fieldEmpty" }),
-            index,
-            setRoundTwoError
-          );
-        }
-        if (!item?.company_shortlisting_start_date) {
-          errorCount++;
-          handleError(
-            "company_shortlisting_start_date",
-            intl.formatMessage({ id: "label.error.fieldEmpty" }),
-            index,
-            setRoundTwoError
-          );
-        }
-      });
-    }
-    if (errorCount) {
-      return false;
-    }
-    return true;
-  };
 
   const handleCancel = () => {
     if (activeTab === "1") {
@@ -553,7 +243,18 @@ const ConsentMarkingContent = ({
   };
 
   const handleSave = () => {
-    if (validate()) {
+    if (
+      validate({
+        activeTab,
+        intl,
+        lastRegistrationTableData,
+        roundOneTableData,
+        roundTwoTableData,
+        setLastRegistrationError,
+        setRoundOneError,
+        setRoundTwoError,
+      })
+    ) {
       if (activeTab === "1") {
         updateLastRegistrationDate({
           body: { data: lastRegistrationTableData },
@@ -648,7 +349,6 @@ const ConsentMarkingContent = ({
   useEffect(() => {
     const activeTab = urlService.getQueryStringValue(ACTIVE_TAB);
     if (registrationDateData?.is_round2_visible) {
-      console.log("is_round2_visible");
       if (!VALID_CONSENT_MARKING_TABS_ID?.threeTab?.includes(activeTab)) {
         urlService.setQueryStringValue(ACTIVE_TAB, 1);
         setActiveTab("1");
@@ -662,7 +362,6 @@ const ConsentMarkingContent = ({
       }
       return;
     }
-    console.log("is_round1_visible");
     if (!VALID_CONSENT_MARKING_TABS_ID.oneTab.includes(activeTab)) {
       urlService.setQueryStringValue(ACTIVE_TAB, 1);
       setActiveTab("1");
@@ -693,7 +392,11 @@ const ConsentMarkingContent = ({
                               }
                               customTimeStyle={styles.customTimeStyle}
                               disabledDate={(current) =>
-                                disabledDate(item.labeIntl, current)
+                                disabledDate(
+                                  item.labeIntl,
+                                  current,
+                                  registrationDatesData
+                                )
                               }
                               isEditable={isRegistrationDateEdit}
                               type="date"
@@ -710,7 +413,11 @@ const ConsentMarkingContent = ({
                                   : null
                               }
                               onChange={(val) => {
-                                handleInputChange(val, item?.labeIntl);
+                                handleInputChange(
+                                  val,
+                                  item?.labeIntl,
+                                  setRegistrationDatesData
+                                );
                               }}
                             />
                           );
