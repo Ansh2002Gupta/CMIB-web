d48dc1a33e641db0b719c83e6afb6d9aa882f423
Kashish8888 <kashish@skizaa.com>
2024-03-14 10:23:58 +0530

    removed unwanted file

--- src/containers/PaymentSettings/PaymentSettings.styles.js
+++
@@ -1,8 +0,0 @@
-import variables from "../../themes/base/styles/variables";
-
-export const classes = {
-  bottomSectionStyle: {
-    alignSelf: "flex-end",
-    marginRight: variables.marginSmall,
-  },
-};
