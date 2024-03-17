fe68f7fe4334defba05f9a3c7ee74bc357e1599b
Kashish8888 <kashish@unthinkable.co>
2024-03-18 01:32:28 +0530

    update candidate details view

--- src/containers/CandidateSettings/CandidateSettingsTemplate.js
+++ src/containers/CandidateSettings/CandidateSettingsTemplate.js
@@ -29,76 +29,74 @@ const CandidateSettingsTemplate = ({
       <Typography className={styles.heading}>
         {intl.formatMessage({ id: "session.candidateSettings" })}
       </Typography>
-      <CustomGrid customStyle={styles.customGridStyle}>
-        {fields?.[0]?.map((_, columnIndex) => (
-          <div className={styles.column} key={columnIndex}>
-            {fields?.map((row, rowIndex) => (
-              <div key={rowIndex} className={styles.row}>
-                <TwoRow
-                  className={styles.gridItem}
-                  topSection={
-                    <Typography className={styles.grayText}>
-                      {intl.formatMessage({
-                        id: `label.${row[columnIndex].headingIntl}`,
-                      })}
-                      {row[columnIndex].rules.isRequired && (
-                        <span className={styles.redText}> *</span>
-                      )}
-                    </Typography>
-                  }
-                  bottomSection={
-                    row[columnIndex].isDateTimePicker ? (
-                      <CustomDateTimePicker
-                        customLabelStyles={styles.inputLabel}
-                        customInputStyle={classes.inputStyle}
-                        type="date"
-                        onChange={(val) => {
-                          handleInputChange(
-                            val ? dayjs(val).format("YYYY-MM-DD") : "",
-                            row[columnIndex].label
-                          );
-                        }}
-                        placeholder={intl.formatMessage({
-                          id: `placeholder.${row[columnIndex].headingIntl}`,
-                        })}
-                        value={row[columnIndex].value}
-                        isEditable
-                      />
-                    ) : (
-                      <CustomInput
-                        controls
-                        value={row[columnIndex].value}
-                        customLabelStyles={styles.inputLabel}
-                        customInputNumberStyles={styles.input}
-                        customContainerStyles={styles.customContainerStyles}
-                        onChange={(val) => {
-                          handleInputChange(val, row[columnIndex].label);
-                        }}
-                        min={TIMER_OF_1_MINUTES}
-                        max={MAX_REGISTRATION_FEE_LENGTH}
-                        type={"inputNumber"}
-                        placeholder={intl.formatMessage({
-                          id: `placeholder.${row[columnIndex].headingIntl}`,
-                        })}
-                        isError={formErrors[row[columnIndex].label]}
-                        errorMessage={
-                          formErrors[row[columnIndex].label]
-                            ? intl.formatMessage({
-                                id: `label.error.${
-                                  formErrors[row[columnIndex].label]
-                                }`,
-                              })
-                            : ""
-                        }
-                      />
-                    )
-                  }
-                />
-              </div>
-            ))}
-          </div>
-        ))}
-      </CustomGrid>
+      {fields?.map((row, rowIndex) => (
+        <CustomGrid
+          key={rowIndex}
+          customStyle={`${styles.customGridStyle} ${styles.row}`}
+        >
+          {row.map((field, columnIndex) => (
+            <TwoRow
+              key={columnIndex}
+              className={`${styles.gridItem} ${styles.column}`}
+              topSection={
+                <Typography className={styles.grayText}>
+                  {intl.formatMessage({
+                    id: `label.${field.headingIntl}`,
+                  })}
+                  {field.rules.isRequired && (
+                    <span className={styles.redText}> *</span>
+                  )}
+                </Typography>
+              }
+              bottomSection={
+                field.isDateTimePicker ? (
+                  <CustomDateTimePicker
+                    customLabelStyles={styles.inputLabel}
+                    customInputStyle={classes.inputStyle}
+                    type="date"
+                    onChange={(val) => {
+                      handleInputChange(
+                        val ? dayjs(val).format("YYYY-MM-DD") : "",
+                        field.label
+                      );
+                    }}
+                    placeholder={intl.formatMessage({
+                      id: `placeholder.${field.headingIntl}`,
+                    })}
+                    value={field.value}
+                    isEditable
+                  />
+                ) : (
+                  <CustomInput
+                    controls
+                    value={field.value}
+                    customLabelStyles={styles.inputLabel}
+                    customInputNumberStyles={styles.input}
+                    customContainerStyles={styles.customContainerStyles}
+                    onChange={(val) => {
+                      handleInputChange(val, field.label);
+                    }}
+                    min={TIMER_OF_1_MINUTES}
+                    max={MAX_REGISTRATION_FEE_LENGTH}
+                    type={"inputNumber"}
+                    placeholder={intl.formatMessage({
+                      id: `placeholder.${field.headingIntl}`,
+                    })}
+                    isError={formErrors[field.label]}
+                    errorMessage={
+                      formErrors[field.label]
+                        ? intl.formatMessage({
+                            id: `label.error.${formErrors[field.label]}`,
+                          })
+                        : ""
+                    }
+                  />
+                )
+              }
+            />
+          ))}
+        </CustomGrid>
+      ))}
       <Typography className={styles.subHeadingStyle}>
         {intl.formatMessage({ id: "label.allow_consent_for_candidate" })}
       </Typography>
