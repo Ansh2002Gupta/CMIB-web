0cb8ffecfaf4aba80b5dc5f7762be38b41b69ad4
Nikhil Sharma <nikhil.sharma@unthinkable.co>
2024-03-18 19:26:36 +0530

    Added label With value component

---
+++ src/components/LabelWithValue/LableWithValue.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Typography } from "antd";
+import commonStyles from "../../common/commonStyles.module.scss";
+
+const LabelWithValue = ({ heading, subHeading, isMandatory = false }) => {
+  return (
+    <div>
+      <Typography className={commonStyles.commonLabelStyle}>
+        {heading}{" "}
+        {isMandatory && <span className={commonStyles.redText}>*</span>}
+      </Typography>
+      <Typography className={commonStyles.commonSubHeadingStyle}>
+        {subHeading}
+      </Typography>
+    </div>
+  );
+};
+export default LabelWithValue;
